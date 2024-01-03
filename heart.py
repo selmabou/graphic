09cf8a2960d628d1662116a6f71bcb8c977facba
selmabou <bouabidisalma07@gmail.com>
2024-01-03 13:00:25 +0100

    init

---
+++ heart.py
@@ -0,0 +1,25 @@
+import math
+from turtle import *
+
+def hearta(k):
+    return 15*math.sin(k)**3
+
+def heartb(k):
+    return 12*math.cos(k)-5*\
+    math.cos(2*k)-2*\
+                math.cos(3*k)-\
+                math.cos(4*k)
+
+speed(1000000)
+bgcolor("black")
+
+for i in range(10000):
+    goto(hearta(i)*20,heartb(i)*20)
+    for j in range(5):
+        color("#f4a261")
+        goto(0,0)
+done()  
+
+                    
+                
+    
